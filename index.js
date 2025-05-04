cf07c8374aafb43e9bf9566618e2c86eed9fe24d
AnySanti <annycarolinysantiago1@gmail.com>
2025-05-04 09:43:51 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector("#botao");
+let mensagem = document.querySelector("#mensagem")
+botao.addEventListener("click",mostrarTexto);
+
+function mostrarTexto(){
+    mensagem.textContent="Bem Vindo!! Que o caminho seja leve, os encontros sejam bons e que cada passo te leve mais perto do que te faz feliz."
+}
+
+    
